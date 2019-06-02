6dad8eefa5fa95ca8cd723ab5f0d96a974959a7f
Jean-Philippe Sirois <jp@veksen.net>
2019-06-02 02:06:36 -0400

    ui: fix container width

--- src/components/Container/styles.tsx
+++ src/components/Container/styles.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components"
 import { ContainerProps } from "./"
 
 export const ContainerWrapper = styled.div<ContainerProps>`
-  max-width: calc(1366px + 20px * 2);
+  max-width: calc(100% - 20px * 2);
+  width: calc(1366px + 20px * 2);
   margin: 0 auto;
   display: flex;
   padding: 0 20px;
--- src/components/Content/styles.tsx
+++ src/components/Content/styles.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components"
 
 export const ContentWrapper = styled.div`
   padding: 20px 0 20px 20px;
-  flex: 1;
+  flex: 1 0 auto;
 `
