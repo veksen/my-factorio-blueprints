532cec433b03a7b2f1284b6daf58f619fe665710
Jean-Philippe Sirois <jp@veksen.net>
2019-06-04 06:15:28 -0400

    chore: set up Apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloClient } from "apollo-client"
+import { HttpLink } from "apollo-link-http"
 import React from "react"
+import { ApolloProvider } from "react-apollo-hooks"
 import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
 import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://my-factorio-blueprints.herokuapp.com/v1/graphql" }),
+  cache: new InMemoryCache()
+})
+
+const AppWrapper = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<AppWrapper />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
